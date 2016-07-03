c2cf19e85e15543baa37d35afaaeb0b830db3a88
Dale Chen <dale@verecho.com>
2016-07-02 23:25:49 -0700

    Update NXOAuth2AccountStore.m

--- Sources/OAuth2Client/NXOAuth2AccountStore.m
+++ Sources/OAuth2Client/NXOAuth2AccountStore.m
@@ -295,7 +295,6 @@ NSString * const kNXOAuth2AccountStoreAccountType = @"kNXOAuth2AccountStoreAccou
         redirectURL:(NSURL *)aRedirectURL
       keyChainGroup:(NSString *)aKeyChainGroup
           tokenType:(NSString *)aTokenType
-          additionalRequestParams:(NSDictionary *)aParams
      forAccountType:(NSString *)anAccountType;
 {
     NSAssert(aKeyChainGroup, @"keyChainGroup must be non-nil");
@@ -309,7 +308,6 @@ NSString * const kNXOAuth2AccountStoreAccountType = @"kNXOAuth2AccountStoreAccou
                                    aTokenURL, kNXOAuth2AccountStoreConfigurationTokenURL,
                                    aTokenType, kNXOAuth2AccountStoreConfigurationTokenType,
                                    aKeyChainGroup, kNXOAuth2AccountStoreConfigurationKeyChainGroup,
-                                   aParams, kNXOAuth2AccountStoreConfigurationAdditionalAuthenticationParameters,
                                    aRedirectURL, kNXOAuth2AccountStoreConfigurationRedirectURL, nil];
     
     if (self.keychainAccessGroup) {
