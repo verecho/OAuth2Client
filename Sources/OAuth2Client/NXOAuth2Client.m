ee112b621778e1c251e9e0aa301424a6fc4aadd0
Dale Chen <dale@verecho.com>
2016-07-03 12:26:20 -0700

    Update NXOAuth2Client.m

--- Sources/OAuth2Client/NXOAuth2Client.m
+++ Sources/OAuth2Client/NXOAuth2Client.m
@@ -253,7 +253,7 @@ NSString * const NXOAuth2ClientConnectionContextTokenRefresh = @"tokenRefresh";
 - (NSURL *)authorizationURLWithRedirectURL:(NSURL *)redirectURL;
 {
     NSMutableDictionary *parameters = [NSMutableDictionary dictionaryWithObjectsAndKeys:
-                                       @"id_token code", @"response_type",
+                                       @"id_token", @"response_type",
                                        clientId, @"client_id",
                                        [redirectURL absoluteString], @"redirect_uri",
                                        @"7362CAEA-9CA5-4B43-9BA3-34D7C303EBA7", @"nonce",
